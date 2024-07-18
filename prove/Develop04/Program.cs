5f92a010d34a1e2f9dd6fcd48ee528176b77cb94
delapaix105 <delapaix2k@gmail.com>
2024-07-18 08:02:24 +0200

    Finished Develop04 Project

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -1,9 +1,191 @@
 using System;
+using System.Collections.Generic;
+using System.Threading;
+
+abstract class MindfulnessActivity
+{
+    protected int duration; // in seconds
+    protected string description;
+
+    public MindfulnessActivity(string description)
+    {
+        this.description = description;
+    }
+
+    public void Start()
+    {
+        Console.WriteLine(description);
+        Console.Write("Enter the duration of the activity in seconds: ");
+        duration = int.Parse(Console.ReadLine());
+        Console.WriteLine("Prepare to begin...");
+        ShowSpinner(3);
+
+        PerformActivity();
+
+        Console.WriteLine("Good job!");
+        Console.WriteLine($"You have completed the activity for {duration} seconds.");
+        ShowSpinner(3);
+    }
+
+    protected abstract void PerformActivity();
+
+    protected void ShowSpinner(int seconds)
+    {
+        for (int i = 0; i < seconds; i++)
+        {
+            Console.Write(".");
+            Thread.Sleep(1000);
+        }
+        Console.WriteLine();
+    }
+
+    protected void ShowCountdown(int seconds)
+    {
+        for (int i = seconds; i > 0; i--)
+        {
+            Console.Write(i + " ");
+            Thread.Sleep(1000);
+        }
+        Console.WriteLine();
+    }
+}
+
+class BreathingActivity : MindfulnessActivity
+{
+    public BreathingActivity() : base("This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing.")
+    {
+    }
+
+    protected override void PerformActivity()
+    {
+        int halfDuration = duration / 2;
+        for (int i = 0; i < halfDuration; i++)
+        {
+            Console.WriteLine("Breathe in...");
+            ShowCountdown(5);
+            Console.WriteLine("Breathe out...");
+            ShowCountdown(5);
+        }
+    }
+}
+
+class ReflectionActivity : MindfulnessActivity
+{
+    private static List<string> prompts = new List<string>
+    {
+        "Think of a time when you stood up for someone else.",
+        "Think of a time when you did something really difficult.",
+        "Think of a time when you helped someone in need.",
+        "Think of a time when you did something truly selfless."
+    };
+
+    private static List<string> questions = new List<string>
+    {
+        "Why was this experience meaningful to you?",
+        "Have you ever done anything like this before?",
+        "How did you get started?",
+        "How did you feel when it was complete?",
+        "What made this time different than other times when you were not as successful?",
+        "What is your favorite thing about this experience?",
+        "What could you learn from this experience that applies to other situations?",
+        "What did you learn about yourself through this experience?",
+        "How can you keep this experience in mind in the future?"
+    };
+
+    public ReflectionActivity() : base("This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.")
+    {
+    }
+
+    protected override void PerformActivity()
+    {
+        Random random = new Random();
+        string prompt = prompts[random.Next(prompts.Count)];
+        Console.WriteLine(prompt);
+        ShowSpinner(5);
+
+        int remainingTime = duration - 5;
+        while (remainingTime > 0)
+        {
+            string question = questions[random.Next(questions.Count)];
+            Console.WriteLine(question);
+            ShowSpinner(5);
+            remainingTime -= 5;
+        }
+    }
+}
+
+class ListingActivity : MindfulnessActivity
+{
+    private static List<string> prompts = new List<string>
+    {
+        "Who are people that you appreciate?",
+        "What are personal strengths of yours?",
+        "Who are people that you have helped this week?",
+        "When have you felt the Holy Ghost this month?",
+        "Who are some of your personal heroes?"
+    };
+
+    public ListingActivity() : base("This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.")
+    {
+    }
+
+    protected override void PerformActivity()
+    {
+        Random random = new Random();
+        string prompt = prompts[random.Next(prompts.Count)];
+        Console.WriteLine(prompt);
+        ShowSpinner(5);
+
+        int remainingTime = duration - 5;
+        int itemCount = 0;
+        Console.WriteLine("Start listing items:");
+
+        while (remainingTime > 0)
+        {
+            string item = Console.ReadLine();
+            itemCount++;
+            remainingTime -= 5;
+        }
+
+        Console.WriteLine($"You listed {itemCount} items.");
+    }
+}
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        while (true)
+        {
+            Console.WriteLine("Choose an activity:");
+            Console.WriteLine("1. Breathing Activity");
+            Console.WriteLine("2. Reflection Activity");
+            Console.WriteLine("3. Listing Activity");
+            Console.WriteLine("4. Exit");
+            Console.Write("Enter your choice: ");
+            int choice = int.Parse(Console.ReadLine());
+
+            MindfulnessActivity activity;
+
+            switch (choice)
+            {
+                case 1:
+                    activity = new BreathingActivity();
+                    break;
+                case 2:
+                    activity = new ReflectionActivity();
+                    break;
+                case 3:
+                    activity = new ListingActivity();
+                    break;
+                case 4:
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice. Please try again.");
+                    continue;
+            }
+
+            activity.Start();
+        }
     }
 }
